a922d83062caabc0c54b41566ae136f4db4ae8b3
Alexander Ting <AlexTing@Alexanders-MacBook-Pro.local>
2016-10-17 14:23:52 -0700

    [Feature] Added style.css file.  Added google search bar to explorer map.

--- client/app/explorer.js
+++ client/app/explorer.js
@@ -1,33 +1,38 @@
-console.log('in explorer module');
 angular.module('explorer', [])
 .controller('mapCtrl', function($scope) {
   var initializeMap = function() {
-  var mapOptions = {
-    zoom: 14,
-    center: new google.maps.LatLng(37.787661, -122.399811),
-    mapTypeId: 'roadmap'
-  }
+    var mapOptions = {
+      zoom: 14,
+      center: new google.maps.LatLng(37.787661, -122.399811),
+      mapTypeId: 'roadmap'
+    }
 
-  $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  $scope.infoWindow = new google.maps.InfoWindow({map: $scope.map});
-  console.log('after instanstiation of map');
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    $scope.infoWindow = new google.maps.InfoWindow({map: $scope.map});
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
 
-      // $scope.infoWindow.setPosition(pos);
-      // $scope.infoWindow.setContent('You here fam.');
-      $scope.map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, $scope.infoWindow, $scope.map.getCenter());
-    });
-  } else {
-    handleLocationError(false, $scope.infoWindow, $scope.map.getCenter());
-  }
-};
-initializeMap();
+        // $scope.infoWindow.setPosition(pos);
+        // $scope.infoWindow.setContent('You here fam.');
+        $scope.map.setCenter(pos);
+      }, function() {
+        handleLocationError(true, $scope.infoWindow, $scope.map.getCenter());
+      });
+    } else {
+      handleLocationError(false, $scope.infoWindow, $scope.map.getCenter());
+    }
+  };
+  initializeMap();
+
+  var initializeSearch = function () {
+    var input = document.getElementById('locSearch');
+    $scope.locSearch = new google.maps.places.SearchBox(input);
+    $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  };
+  initializeSearch();
 })
