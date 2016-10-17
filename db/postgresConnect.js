edc840f2e7692f0dd1da120608cd72f6a80f35c4
ashermanwmf <shermanandrew50@gmail.com>
2016-10-17 13:48:24 -0700

    [Feature] - added a schema file and moved schema from postgresconnect file

--- db/postgresConnect.js
+++ db/postgresConnect.js
@@ -15,25 +15,6 @@ const pool = new pg.Pool(config);
 
 module.exports = pool;
 
-// create table events(
-// 	id serial,
-// 	name varchar,
-// 	host varchar,
-// 	description varchar,
-// 	lat decimal,
-// 	long decimal,
-// 	Primary key (id)
-// );
-
-// create table dates(
-// 	id serial,
-// 	start_date varchar,
-// 	end_date varchar,
-// 	fk_event int,
-// 	primary key (id),
-// 	foreign key (fk_event) references public.events(id) 
-// );
-
 /* SAMPLE QUERY FOR DATABSE */
 
 // promise based
