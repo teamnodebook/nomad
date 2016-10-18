fa08f917de7a2f0dbbdc932d22e1215c76b789da
melbee <melba@transferhero.com>
2016-10-18 15:22:12 -0700

    [Update] updated pool promise chain

--- db/seed.js
+++ db/seed.js
@@ -3,20 +3,25 @@ const _ = require('underscore');
 
 const data = require('../data.json');
 
-new Promise((resolve, reject) =>{
+const cp = new Promise((resolve, reject) =>{
 	pool.connect(function(err, client, done) {
 	  if(err) {
 	  	reject(err);
 	  }
 	  console.log('we have a client and are ready to query');
-	  client.query('drop table public.events, public.dates', 
-	  						(err, result) =>{
-									//console.log(err, 'check error drop table')
-									//console.log(result, ' result from insert statement')
-								});
 	  resolve(client);
 	});
-}).then((client) =>{
+});
+
+cp.then((client) => {
+  client.query('drop table public.events, public.dates', 
+						(err, result) =>{
+							console.log(err, 'check error drop table')
+							console.log(result, ' result from insert statement')
+						});
+});
+
+cp.then((client) =>{	
 	// Create tables (look at schema in Schema.sql)	
 	client.query(
 		`create table public.events(
@@ -39,39 +44,39 @@ new Promise((resolve, reject) =>{
 		);`, 
 			(err, result) =>{
 				console.log(err, 'check error create table')
-				console.log(result, ' result from insert statement create table')
-
-				let insertTimes = (client, obj) =>{
-					_.each(obj.time, (time) =>{
-						client.query(`insert into public.dates
-												(start_date, end_date, fk_event)
-												values ('${time.start}',
-												'${time.end}',
-												(select id from public.events where name='${obj.info.name.replace(/(')/g, '\"')}}' and lat=${obj.location.lat} and long=${obj.location.long}))`,
-												(err, result) =>{
-													console.log(err, 'check error insert times')
-													console.log(result, ' result from insert statement insert times')
-												});
-					});
-				}; 
-
-				_.each(data, (obj) =>{
-				client.query(`insert into public.events
-												(name, host, description, lat, long)
-												values ('${obj.info.name.replace(/(')/g, '\"')}}',
-												'${obj.info.host.replace(/(')/g, '\"')}}',
-												'${obj.info.description.replace(/(')/g, '\"')}}',
-												${obj.location.lat},
-												${obj.location.long})`,
-											(err, result) =>{
-												console.log(obj.info.name)
-												console.log(err, 'check error insert event')
-												console.log(result, ' result from insert statement insert event')
-												insertTimes(client, obj);
-												return;
-											});
-				});
+				console.log(result, ' result from insert statement create table')				
 			});
-}).then((client) =>{
-	console.log(client, 'chained then')
+})
+
+cp.then((client) =>{
+	let insertTimes = (client, obj) =>{
+		_.each(obj.time, (time) =>{
+			client.query(`insert into public.dates
+									(start_date, end_date, fk_event)
+									values ('${time.start}',
+									'${time.end}',
+									(select id from public.events where name='${obj.info.name.replace(/(')/g, '\"')}}' and lat=${obj.location.lat} and long=${obj.location.long}))`,
+									(err, result) =>{
+										console.log(err, 'check error insert times')
+										console.log(result, ' result from insert statement insert times')
+									});
+		});
+	}; 
+
+	_.each(data, (obj) =>{
+		client.query(`insert into public.events
+										(name, host, description, lat, long)
+										values ('${obj.info.name.replace(/(')/g, '\"')}}',
+										'${obj.info.host.replace(/(')/g, '\"')}}',
+										'${obj.info.description.replace(/(')/g, '\"')}}',
+										${obj.location.lat},
+										${obj.location.long})`,
+									(err, result) =>{
+										console.log(obj.info.name)
+										console.log(err, 'check error insert event')
+										console.log(result, ' result from insert statement insert event')
+										insertTimes(client, obj);
+										return;
+									});
+	});
 });
