73b64a5a8d9806b854166bf047bc8f56d9c75848
melbee <melba@transferhero.com>
2016-10-22 11:30:53 -0700

    resolved merge conflict

--- client/app/landing.js
+++ client/app/landing.js
@@ -8,26 +8,31 @@ angular.module('landingPage', [])
 		if(input.value){
 			$location.url("nomad");
 		}			  
-	  console.log("location: ", $scope.locSearch.getPlaces())
+
+		var place = $scope.locSearch.getPlaces()	  
+	  console.log("lat: ", place[0].geometry.location.lat())
+	  console.log("long: ", place[0].geometry.location.lng())
 	  //need to look up what geocoder does - is it getting lat/long
 	  //need to look at alex's code to also get radius
 	  // make request 
-	  var geocoder = new google.maps.Geocoder();  
-	  geocoder.geocode( {'address': input}, function(results, status) {
-	    if (status === 'OK') {        
-	      lat = results[0].geometry.location.lat();
-	      long = results[0].geometry.location.lng();
-	      params.loc = input;
-	      markers[0].setMap(null);
-	      markers.shift();
-	      markers.push(new google.maps.Marker({
-	                    position: results[0].geometry.location,
-	                    map: nomadMap
-	                  }));
-	    } else {
-	      alert('geocode not successful ' + status);
-	    }
-	  });
+
+	  // var geocoder = new google.maps.Geocoder();  
+	  // geocoder.geocode( {'address': input}, function(results, status) {
+	  //   if (status === 'OK') {        
+	  //     lat = results[0].geometry.location.lat();
+	  //     long = results[0].geometry.location.lng();
+	  //     params.loc = input;
+	  //     markers[0].setMap(null);
+	  //     markers.shift();
+	  //     markers.push(new google.maps.Marker({
+	  //                   position: results[0].geometry.location,
+	  //                   map: nomadMap
+	  //                 }));
+	  //   } else {
+	  //     alert('geocode not successful ' + status);
+	  //   }
+	  // });
+
 	}
 // look at geocoder ; add default radius in alex / jemils code
 
