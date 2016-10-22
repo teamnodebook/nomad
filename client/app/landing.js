e002d1ce8698795e50261633d2f69a752912b15a
melbee <melba@transferhero.com>
2016-10-22 11:28:39 -0700

    resolved merge conflict

--- client/app/landing.js
+++ client/app/landing.js
@@ -1,8 +1,34 @@
 angular.module('landingPage', [])
-.controller('landingCtrl', ($scope) => {
-  
-  const input = document.getElementById('locSearch');
-  $scope.locSearch = new google.maps.places.SearchBox(input);
-  console.log("location search from landing: ", $scope.locSearch);
+.controller('landingCtrl', ($scope, $location) => {  
+	const input = document.getElementById('locSearch');
+  var submit = document.getElementById('location-submit-button');
+	$scope.locSearch = new google.maps.places.SearchBox(input);  
+	
+	$scope.setMapCenter = () => {  			 		
+		if(input.value){
+			$location.url("nomad");
+		}			  
+	  console.log("location: ", $scope.locSearch.getPlaces())
+	  //need to look up what geocoder does - is it getting lat/long
+	  //need to look at alex's code to also get radius
+	  // make request 
+	  var geocoder = new google.maps.Geocoder();  
+	  geocoder.geocode( {'address': input}, function(results, status) {
+	    if (status === 'OK') {        
+	      lat = results[0].geometry.location.lat();
+	      long = results[0].geometry.location.lng();
+	      params.loc = input;
+	      markers[0].setMap(null);
+	      markers.shift();
+	      markers.push(new google.maps.Marker({
+	                    position: results[0].geometry.location,
+	                    map: nomadMap
+	                  }));
+	    } else {
+	      alert('geocode not successful ' + status);
+	    }
+	  });
+	}
 // look at geocoder ; add default radius in alex / jemils code
+
 });
