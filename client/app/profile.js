70a14d125bb435d07386735fed9a2dfe244dda38
Bruna Enos <bruna_bsm@icloud.com>
2016-10-27 13:41:40 -0700

    nomadProfile module

--- client/app/profile.js
+++ client/app/profile.js
@@ -1,7 +1,15 @@
 angular.module('nomadProfile',['ngPassword'])
 
-.controller('profileCtrl', function($scope, params, $http, $location){
+.controller('profileCtrl', function($scope, params, $http, $location,$modalInstance){
 	$scope.username;
 	$scope.email;
 	$scope.password;
+	$scope.addUser = () => {
+	  // var newUser = {
+	  // 	username: $scope.username,
+	  //   email: $scope.email,
+	  //   password: $scope.password
+	  //   }
+	    console.log('Hello');
+	}
 })
